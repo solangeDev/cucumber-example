cac8e077cdb4dce4774d1bd10d25cc9cfbb63a3d
Carla Ramirez <carla.ramirez@pranical.com>
2024-08-21 15:37:51 -0400

    structure base

---
+++ features/example.feature
@@ -0,0 +1,5 @@
+Feature: Example feature
+
+  Scenario: Simple scenario
+    Given I open Google
+    Then the title should be "Google"
---
+++ reporter.js
@@ -0,0 +1,19 @@
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+  theme: 'bootstrap',
+  jsonFile: 'reports/cucumber_report.json',
+  output: 'reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  launchReport: true,
+  metadata: {
+    "App Version": "1.0.0",
+    "Test Environment": "STAGING",
+    "Browser": "Chrome 89",
+    "Platform": "Windows 10",
+    "Parallel": "Scenarios",
+    "Executed": "Local"
+  }
+};
+
+reporter.generate(options);
